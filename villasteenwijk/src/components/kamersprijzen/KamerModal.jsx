f420415c75929e782e499944f96c12fa50d32065
Roman - <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>
2023-11-28 01:13:26 +0100

    Initial room array created

--- villasteenwijk/src/components/kamersprijzen/KamerModal.jsx
+++ villasteenwijk/src/components/kamersprijzen/KamerModal.jsx
@@ -5,6 +5,8 @@ import { IoPeopleSharp } from "react-icons/io5";
 import { FaPeopleGroup } from "react-icons/fa6";
 import { FaExclamationTriangle } from "react-icons/fa";
 import { PiForkKnifeBold } from "react-icons/pi";
+
+//Kamer 1 Hildo Krop
 import Pic1 from "../../assets/hildokrop/Pic1.webp";
 import Pic2 from "../../assets/hildokrop/Pic2.webp";
 import Pic3 from "../../assets/hildokrop/Pic3.webp";
@@ -29,6 +31,43 @@ const KamerModal = () => {
   };
 
   const imageSources = [Pic1, Pic2, Pic3, Pic4, Pic5, Pic6, Pic7, Pic8];
+
+  const kamers = {
+    kamer1: {
+      hero: "Hildo krop",
+      "2personen": 109,
+      "3personen": 0,
+      copy1:
+        "Vernoemd naar de in Steenwijk geboren beroemde kunstenaar en beeldhouwer Hildo Krop, de Stadsbeeldhouwer van Amsterdam.",
+      copy2:
+        "Deze prachtige, 2-persoons, lichte, kamer met Engels behang, rijke, velourse, gordijnstoffen en zachte vloerbedekking bevind zich op de 2e verdieping van Villa Steenwyck met daardoor een prachtig uitzicht over vestingstad Steenwijk en de Steenwijker toren.",
+    },
+    voorziening: [
+      "Luxe Boxspring bedden (2 bedden tegen elkaar aan, 180x200cm met luxe topper)",
+      "De bedden zijn ook los van elkaar te zetten, als 2 1-persoons bedden van 90x200cm. Indien gewenst",
+      "Satijnen beddengoed en badtextiel van “De witte Lietaer”",
+      "Eet setje om uw ontbijt te nuttigen of een spelletje te spelen",
+      "Eet setje om uw ontbijt te nuttigen of een spelletje te spelen",
+      "Luxe Philips Ambilight TV met Netflix en kabel TV",
+      "Gratis super snelle wifi",
+      "Voorzien van Daikin warmtepomp airconditioners voor het milieu bewust koelen en verwarmen van uw kamer met ingebouwde flash-streamer luchtreiniging tegen stof, pollen, virussen, bacteriën en allergenen, Voor een altijd frisse “berglucht” in uw kamer",
+      "Gedeelde badkamer met toilet, bidet, wastafel en luxe regendouche, met per kamer een plankje voor uw toilet spullen",
+      "Gratis koffie (Nespresso) en thee faciliteiten",
+      "Koelkast met daarin koel, plat water",
+      "2 Luxe, zachte badjassen van “De Witte Lietaer”",
+    ],
+    images: [
+      "../../assets/hildokrop/Pic1.webp",
+      "../../assets/hildokrop/Pic2.webp",
+      "../../assets/hildokrop/Pic3.webp",
+      "../../assets/hildokrop/Pic4.webp",
+      "../../assets/hildokrop/Pic5.webp",
+      "../../assets/hildokrop/Pic6.webp",
+      "../../assets/hildokrop/Pic7.webp",
+      "../../assets/hildokrop/Pic8.webp",
+    ],
+  };
+
   return (
     <div className={stl.kamermodal}>
       <div className={stl.modal}>
@@ -36,7 +75,7 @@ const KamerModal = () => {
         <div className={stl.imgWrap}>
           <img
             src={imageSources[activeButton]}
-            alt="Hildo Krop Kamer"
+            alt="B&B Kamer"
             className={stl.roomImg}
           />
           <div className={stl.buttonBox}>
@@ -134,6 +173,30 @@ const KamerModal = () => {
                 </li>
                 <li className={stl.voorziening}>
                   Eet setje om uw ontbijt te nuttigen of een spelletje te spelen
+                </li>
+                <li className={stl.voorziening}>
+                  Luxe Philips Ambilight TV met Netflix en kabel TV
+                </li>
+                <li className={stl.voorziening}>Gratis super snelle wifi</li>
+                <li className={stl.voorziening}>
+                  Voorzien van Daikin warmtepomp airconditioners voor het milieu
+                  bewust koelen en verwarmen van uw kamer met ingebouwde
+                  flash-streamer luchtreiniging tegen stof, pollen, virussen,
+                  bacteriën en allergenen, Voor een altijd frisse “berglucht” in
+                  uw kamer
+                </li>
+                <li className={stl.voorziening}>
+                  Gedeelde badkamer met toilet, bidet, wastafel en luxe
+                  regendouche, met per kamer een plankje voor uw toilet spullen
+                </li>
+                <li className={stl.voorziening}>
+                  Gratis koffie (Nespresso) en thee faciliteiten
+                </li>
+                <li className={stl.voorziening}>
+                  Koelkast met daarin koel, plat water
+                </li>
+                <li className={stl.voorziening}>
+                  2 Luxe, zachte badjassen van “De Witte Lietaer”
                 </li>
               </ul>
             </div>
