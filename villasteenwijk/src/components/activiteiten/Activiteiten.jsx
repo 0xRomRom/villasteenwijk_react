6f0d0ac946fe4b677b6e7108048eee489bf47766
0xRomRom <roman0811@live.nl>
2023-11-28 05:11:43 +0100

    Rent a chopper component created

--- villasteenwijk/src/components/activiteiten/Activiteiten.jsx
+++ villasteenwijk/src/components/activiteiten/Activiteiten.jsx
@@ -1,8 +1,10 @@
 import stl from "./Activiteiten.module.css";
 import { motion as m } from "framer-motion";
+import { Navigate, useNavigate } from "react-router-dom";
 
 //Kamer 1 Hildo Krop
 const Activiteiten = () => {
+  const navigate = useNavigate();
   return (
     <div className={stl.portfolio}>
       <m.header
@@ -29,7 +31,25 @@ const Activiteiten = () => {
           </m.h2>
         </div>
       </m.header>
-      <div className={stl.contentWrap}></div>
+      <div className={stl.contentWrap}>
+        <div className={stl.chopperModal}>
+          <div className={stl.innerChopperModal}>
+            <h2>Huur een E-Chopper</h2>
+            <div className={stl.conditionsList}>
+              <span className={stl.conditionSpan}>€35,- voor 4 uur</span>
+              <span className={stl.conditionSpan}>€50,- voor 1 dag</span>
+              <span className={stl.conditionSpan}>1 E-Chopper per persoon</span>
+              <span className={stl.conditionSpan}>Max bereik: 40KM</span>
+              <button
+                className={stl.bookCta}
+                onClick={() => navigate("/contact")}
+              >
+                Book
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
