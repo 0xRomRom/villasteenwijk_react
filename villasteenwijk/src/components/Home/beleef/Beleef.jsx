89b5247b5cf333620d30195244a3be61d9bd22cc
Roman - <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>
2023-11-29 23:45:32 +0100

    Hashrouter added

--- villasteenwijk/src/components/Home/beleef/Beleef.jsx
+++ villasteenwijk/src/components/Home/beleef/Beleef.jsx
@@ -20,7 +20,7 @@ const Beleef = () => {
           initial={{ opacity: 0, y: "-20px" }}
           whileInView={{ opacity: 0.2, y: "0px" }}
           transition={{ duration: 1.25 }}
-          src="./Landing/2.webp"
+          src="./landing/2.webp"
           alt="Villa Boutique Hotel Steenwijk"
           className={stl.vilaBg}
         />
