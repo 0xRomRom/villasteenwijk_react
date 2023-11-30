d926321aa7f8b331349102925f7e92b3ec5162bf
Roman - <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>
2023-11-30 17:45:50 +0100

    Hide main content when modal open

--- villasteenwijk/src/components/kamersprijzen/KamersPrijzen.jsx
+++ villasteenwijk/src/components/kamersprijzen/KamersPrijzen.jsx
@@ -26,33 +26,41 @@ const KamersPrijzen = () => {
       >
         Kamers & Prijzen
       </m.h1>
-      <div className={stl.fiveDoorGrid}>
-        <div className={stl.doorTile} onClick={() => openModal(1)}>
-          <img src={door} alt="Deur" className={stl.doorImg} />
-          <span className={stl.doorName}>1. Hildo Krop</span>
-        </div>
-        <div className={stl.doorTile} onClick={() => openModal(2)}>
-          <img src={door} alt="Deur" className={stl.doorImg} />
-          <span className={stl.doorName}>2. Tromp Meesters</span>
-        </div>
-        <div className={stl.doorTile} onClick={() => openModal(3)}>
-          <img src={door} alt="Deur" className={stl.doorImg} />
-          <span className={stl.doorName}>3. Kleibeuker</span>
-        </div>
-        <div className={stl.doorTile} onClick={() => openModal(4)}>
-          <img src={door} alt="Deur" className={stl.doorImg} />
-          <span className={stl.doorName}>4. Johan van den Kornput</span>
-        </div>
-        <div className={stl.doorTile} onClick={() => openModal(5)}>
-          <img src={door} alt="Deur" className={stl.doorImg} />
-          <span className={stl.doorName}>5. Giethoorn</span>
-        </div>
-      </div>
+      {!kamerKeuze && (
+        <>
+          <div className={stl.fiveDoorGrid}>
+            <div className={stl.doorTile} onClick={() => openModal(1)}>
+              <img src={door} alt="Deur" className={stl.doorImg} />
+              <span className={stl.doorName}>1. Hildo Krop</span>
+            </div>
+            <div className={stl.doorTile} onClick={() => openModal(2)}>
+              <img src={door} alt="Deur" className={stl.doorImg} />
+              <span className={stl.doorName}>2. Tromp Meesters</span>
+            </div>
+            <div className={stl.doorTile} onClick={() => openModal(3)}>
+              <img src={door} alt="Deur" className={stl.doorImg} />
+              <span className={stl.doorName}>3. Kleibeuker</span>
+            </div>
+            <div className={stl.doorTile} onClick={() => openModal(4)}>
+              <img src={door} alt="Deur" className={stl.doorImg} />
+              <span className={stl.doorName}>4. Johan van den Kornput</span>
+            </div>
+            <div className={stl.doorTile} onClick={() => openModal(5)}>
+              <img src={door} alt="Deur" className={stl.doorImg} />
+              <span className={stl.doorName}>5. Giethoorn</span>
+            </div>
+          </div>
 
-      <div className={stl.appartmentGrid} onClick={() => openModal(6)}>
-        <img src={appartment} alt="Appartment" className={stl.appartmentImg} />
-        <span className={stl.appTitle}>Appartement Villa Steenwyck</span>
-      </div>
+          <div className={stl.appartmentGrid} onClick={() => openModal(6)}>
+            <img
+              src={appartment}
+              alt="Appartment"
+              className={stl.appartmentImg}
+            />
+            <span className={stl.appTitle}>Appartement Villa Steenwyck</span>
+          </div>
+        </>
+      )}
     </main>
   );
 };
